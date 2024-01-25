e4444af5a274630dd41a292e675eec478e7c8828
Rosex5711 <2904440771@qq.com>
2024-01-25 18:26:21 +0800

    loss curve and psnr

--- psnr.py
+++ psnr.py
@@ -2,21 +2,22 @@ from PIL import Image
 import numpy as np
 import math
 
-def cal_psnr(img1, img2):
+def cal_psnr(img1, img2, data_range):
    '''
-   img1, img2:  m x n x 3, input images
-   return:      100 if the two images are nearly the same; else psnr (<=100)
+   calculate the psnr of two images
+   Args:
+      img1, img2:  ndarray, m x n x 3
+      data_range:  maximum possible pixel value of the image
+      return:      100 if the two images are nearly the same; else psnr (<=100)
    '''
-   arr1 = np.array(img1)
-   arr2 = np.array(img2)
-   assert arr1.shape == arr2.shape
-   mse = np.mean( (arr1/255. - arr2/255.) ** 2 )
+   assert img1.shape == img2.shape
+   mse = np.mean( (img1/data_range - img2/data_range) ** 2 )
    if mse < 1.0e-10:
       return 100
-   PIXEL_MAX = 1
+   PIXEL_MAX = 1.
    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 if __name__ == '__main__':
-   img1 = Image.open("background.jpg")
-   img2 = Image.open("background2.jpg")
-   print(cal_psnr(img1, img2))
+   img1 = Image.open("C:/Users/Rose Xue/Desktop/background.jpg")
+   img2 = Image.open("C:/Users/Rose Xue/Desktop/background2.jpg")
+   print(cal_psnr(np.array(img1)/255., np.array(img2)/255., 1))
